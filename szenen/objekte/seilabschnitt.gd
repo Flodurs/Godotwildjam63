ab48d6050ec3c10a2fe90ddf28dc00efa7c2ac29
Flodurs <76902177+Flodurs@users.noreply.github.com>
2023-11-12 00:27:37 +0100

    seil

--- szenen/objekte/seilabschnitt.gd
+++ szenen/objekte/seilabschnitt.gd
@@ -1,22 +1,31 @@
 extends Area2D
-
+var hitflag = 0
+var primus = 1
+var hitPoint = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$CollisionShape2D.get_shape().a=$Line2D.points[0] 
-	$CollisionShape2D.get_shape().b=$Line2D.points[1]
+	if $Line2D.points.size() == 2:
+		$CollisionShape2D.get_shape().a=$Line2D.points[0] 
+		$CollisionShape2D.get_shape().b=$Line2D.points[1]
+
+func getHitFlag():
+	if hitflag==1:
+		hitflag=0
+		return 1
+	return 0
+func getHitPoint():
+	return hitPoint
 
 
 
 
-
-
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	pass
 		
+	
+
 	
