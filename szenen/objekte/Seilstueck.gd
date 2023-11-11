0f7c52a1bc3c44f58c70d50bfc6a4852d94fb8ee
Fabian L <f.linial@gmx.de>
2023-11-11 14:41:13 +0100

    bunte fäden

---
+++ szenen/objekte/Seilstueck.gd
@@ -0,0 +1,18 @@
+extends Line2D
+
+var start:Vector2 = Vector2.ZERO
+var ende:Vector2 = Vector2.ZERO
+var richtung:Vector2 = Vector2.ZERO
+
+
+func _ready():
+	start = get_point_position(0)  
+	ende = get_point_position(1)  
+	richtung = ende-start
+	
+func platzieren(p1:Vector2, p2:Vector2):
+	pass
+
+func getYAtX(x:int) -> int:
+	var y = start.y + richtung.y*((x - start.x)/richtung.x)
+	return y
