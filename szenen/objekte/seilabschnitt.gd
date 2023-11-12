68d65190ae1087afa5366f1ad5f8f4a66455e042
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2023-11-12 16:54:19 +0100

    amagat

--- szenen/objekte/seilabschnitt.gd
+++ szenen/objekte/seilabschnitt.gd
@@ -4,17 +4,19 @@ var ende:Vector2 = Vector2.ZERO
 var richtung:Vector2 = Vector2.ZERO
 
 func _ready():
-#	start = $Line2D.get_point_position(0)  
-#	ende = $Line2D.get_point_position(1)  
-#	richtung = ende-start
 	
 	add_to_group("Cutable")
+	
+func setup():
+	start = $Line2D.get_point_position(0)  
+	ende = $Line2D.get_point_position(1)  
+	richtung = ende-start
 
 func add_point(point:Vector2):
 	$Line2D.add_point(point)
 
 func getVec() -> Vector2:
-	return $Line2D.get_point_position(1) - $Line2D.get_point_position(0)
+	return ($Line2D.get_point_position(1) - $Line2D.get_point_position(0))
 
 #func getYAtX(x:int) -> int:
 #	var y = start.y + richtung.y*((x - start.x)/richtung.x)
