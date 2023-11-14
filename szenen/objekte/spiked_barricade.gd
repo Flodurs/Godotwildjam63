e3d8671df0f0f5a3ca9df5901a5aee4e5e71ab73
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2023-11-14 18:32:40 +0100

    cutTimer

--- szenen/objekte/spiked_barricade.gd
+++ szenen/objekte/spiked_barricade.gd
@@ -5,6 +5,8 @@ extends StaticBody2D
 func _on_area_2d_area_entered(area):
 	for i in area.get_groups():
 		if i == "Cutable":
+			$cutTimer.start()
+			await $cutTimer.timeout
 			area.on_wireCut()
 #			print("CUT!!")
 			for schalter in get_tree().get_nodes_in_group("Schalter"):
